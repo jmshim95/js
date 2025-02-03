42f52487859f7117c19b1a624e38c3a688c23427
jmshim <jmshim@hecto.co.kr>
2025-02-03 09:30:17 +0900

    체크박스 제어 개선

--- checkbox/checkbox.js
+++ checkbox/checkbox.js
@@ -1,8 +1,8 @@
 class Checkbox {
-    _chkId;
-    _chkName;
-    _isRequired = true;
-    _isVisible = true;
+    #chkId;
+    #chkName;
+    #isRequired = true;
+    #isVisible = true;
 
     /**
      *
@@ -12,54 +12,61 @@ class Checkbox {
      * @param {boolean} isVisible 노출되고 있는 체크박스만 선택할것인지
      */
     constructor({chkId = "chkAll", chkName= "chk", isRequired = this.isRequired, isVisible = this.isVisible} = {}) {
-        this._chkId = chkId;
-        this._chkName = chkName;
-        this._isRequired = isRequired;
-        this._isVisible = isVisible;
+        this.#chkId = chkId;
+        this.#chkName = chkName;
+        this.#isRequired = isRequired;
+        this.#isVisible = isVisible;
 
-        this.initCheckboxEvent();
+        this.#initCheckboxEvent();
     }
 
     get chkId() {
-        return this._chkId;
+        return this.#chkId;
     }
 
     set chkId(value) {
-        this._chkId = value;
-        this.initCheckboxEvent();
+        this.#chkId = value;
+        this.#initCheckboxEvent();
     }
 
     get chkName() {
-        return this._chkName;
+        return this.#chkName;
     }
 
     set chkName(value) {
-        this._chkName = value;
-        this.initCheckboxEvent();
+        this.#chkName = value;
+        this.#initCheckboxEvent();
     }
 
     get isRequired() {
-        return this._isRequired;
+        return this.#isRequired;
     }
 
+
+    /**
+     * @param {boolean} value
+     */
     set isRequired(value) {
-        this._isRequired = value;
-        this.initCheckboxEvent();
+        this.#isRequired = value === true;
+        this.#initCheckboxEvent();
     }
 
 
     get isVisible() {
-        return this._isVisible;
+        return this.#isVisible;
     }
 
+    /**
+     * @param {boolean} value
+     */
     set isVisible(value) {
-        this._isVisible = value;
-        this.initCheckboxEvent();
+        this.#isVisible = value === true;
+        this.#initCheckboxEvent();
     }
 
-    initCheckboxEvent() {
-        this.initMainCheckbox();
-        this.initSubCheckboxes();
+    #initCheckboxEvent() {
+        this.#initMainCheckbox();
+        this.#initSubCheckboxes();
     }
 
     getMainCheckbox() {
@@ -67,69 +74,30 @@ class Checkbox {
     }
 
     /**
+     * @param isChecked 체크된 체크박스 선택
      * @param isRequired 필수 체크박스 선택
-     * @param checkedOnly 체크된 체크박스 선택
      * @param isVisible 화면상 보이는 체크박스 선택
-     * @return {NodeListOf<HTMLElement>|NodeListOf<Element>}
      */
-    getSubCheckboxes({checkedOnly = false, isRequired = this.isRequired, isVisible = this.isVisible} = {}) {
-        let checkboxes = [];
-        let filteredCheckboxes = [];
+    getSubCheckboxes({isChecked = false, isRequired = this.isRequired, isVisible = this.isVisible} = {}) {
+        let selector = `input[name='${this.chkName}']`;
+        if (isRequired) selector += `:required`;
+        if (isChecked) selector += `:checked`;
 
-        if (isRequired && checkedOnly) {
-            checkboxes = document.querySelectorAll(`input[name='${this.chkName}']:required:checked`);
+        let checkboxes = Array.from(document.querySelectorAll(selector));
 
-            checkboxes.forEach(e => {
-                if (e.checkVisibility()) {
-                    filteredCheckboxes.push(e);
-                }
-            });
-        } else if (isRequired) {
-            checkboxes = document.querySelectorAll(`input[name='${this.chkName}']:required`);
-
-            checkboxes.forEach(e => {
-                if (e.checkVisibility()) {
-                    filteredCheckboxes.push(e);
-                }
-            });
-        } else  if (checkedOnly) {
-            checkboxes = document.querySelectorAll(`input[name='${this.chkName}']:checked`);
-
-            checkboxes.forEach(e => {
-                if (e.checkVisibility()) {
-                    filteredCheckboxes.push(e);
-                }
-            });
-        } else {
-            checkboxes = document.querySelectorAll(`input[name='${this.chkName}']`);
-
-            checkboxes.forEach(e => {
-                if (e.checkVisibility()) {
-                    filteredCheckboxes.push(e);
-                }
-            });
-        }
-
-        if (isVisible) {
-            return filteredCheckboxes;
-        } else {
-            return checkboxes;
-        }
+        return isVisible ? checkboxes.filter(e => e.checkVisibility()) : checkboxes;
     }
 
     // 모두 체크되었는지
     isAllChecked() {
-        if(this.getSubCheckboxes().length !== 0){
-            return this.getSubCheckboxes().length === this.getSubCheckboxes({checkedOnly: true}).length;
-        } else {
-            return false;
-        }
+        const allCheckboxes = this.getSubCheckboxes();
+        return allCheckboxes.length > 0 && allCheckboxes.every(cb => cb.checked);
     }
 
     /**
      * @description 전체선택 버튼클릭
      */
-    initMainCheckbox() {
+    #initMainCheckbox() {
         const mainCheckbox = this.getMainCheckbox();
 
         mainCheckbox.onchange = () => {
@@ -144,84 +112,64 @@ class Checkbox {
     /**
      * @description 개별 체크박스 모두 체크되면 전체선택 체크
      */
-    initSubCheckboxes() {
-        const self = this;
-        const mainCheckbox = self.getMainCheckbox();
-        const subCheckboxes = self.getSubCheckboxes();
+    #initSubCheckboxes = () =>  {
+        const mainCheckbox = this.getMainCheckbox();
+        const subCheckboxes = this.getSubCheckboxes();
 
         subCheckboxes.forEach(e => {
             e.onchange = () => {
-                mainCheckbox.checked = self.isAllChecked();
+                mainCheckbox.checked = this.isAllChecked();
             };
         });
     }
 
-    /**
-     * @description 전체선택(선택항목 제외)
-     */
-    checkAll() {
+    setAllChecked(state) {
         const mainCheckbox = this.getMainCheckbox();
         const subCheckboxes = this.getSubCheckboxes();
 
-        mainCheckbox.checked = true;
-        subCheckboxes.forEach(e => {
-            e.checked = true;
-        });
+        if (!mainCheckbox) return;
+
+        mainCheckbox.checked = state;
+        subCheckboxes.forEach(cb => cb.checked = state);
     }
 
-    /**
-     * @description 전체선택해제(선택항목 포함)
-     */
-    uncheckAll() {
-        const self = this;
-        const mainCheckbox = self.getMainCheckbox();
-        const subCheckboxes = self.getSubCheckboxes(); // 모든 체크박스
+    checkAll() {
+        this.setAllChecked(true);
+    }
 
-        mainCheckbox.checked = false;
-        subCheckboxes.forEach(e => {
-            e.checked = false;
-        });
+    uncheckAll() {
+        this.setAllChecked(false);
+    }
+
+    setDisabled(state) {
+        const mainCheckbox = this.getMainCheckbox();
+        const subCheckboxes = this.getSubCheckboxes();
+
+        if (!mainCheckbox) return;
+
+        mainCheckbox.disabled = state;
+        subCheckboxes.forEach(cb => cb.disabled = state);
     }
 
     /**
      * 클릭 비활성화
      */
     disableAll() {
-        const self = this;
-        const mainCheckbox = self.getMainCheckbox();
-        const subCheckboxes = self.getSubCheckboxes(); // 모든 체크박스
-
-        mainCheckbox.disabled = true;
-        subCheckboxes.forEach(e => {
-            e.disabled = true;
-        });
+        this.setDisabled(true);
     }
 
     /**
      * 클릭 활성화
      */
     enableAll() {
-        const self = this;
-        const mainCheckbox = self.getMainCheckbox();
-        const subCheckboxes = self.getSubCheckboxes(); // 모든 체크박스
-
-        mainCheckbox.disabled = false;
-        subCheckboxes.forEach(e => {
-            e.disabled = false;
-        });
+        this.setDisabled(false);
     }
+
 
     /**
      * 체크된 항목들의 값 배열화
      */
     getValues() {
-        const subCheckboxes = this.getSubCheckboxes({checkedOnly: true});
-        const values = [];
-
-        subCheckboxes.forEach(e => {
-            values.push(e.value)
-        });
-
-        return values;
+        return this.getSubCheckboxes({ isChecked: true }).map(cb => cb.value);
     }
 }
